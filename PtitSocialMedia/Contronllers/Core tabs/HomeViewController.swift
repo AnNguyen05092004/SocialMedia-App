85f7ed5a8b26fbdf268ca3e50d1c3bfe1c0b211c
AnNguyen05092004 <an05092004@gmail.com>
2025-03-28 10:44:36 +0700

    First commit

---
+++ PtitSocialMedia/Contronllers/Core tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PtitSocialMedia
+//
+//  Created by An Nguyen on 27/03/2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PtitSocialMedia/Contronllers/Other/PublishPostViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PublishPostViewController.swift
+//  PtitSocialMedia
+//
+//  Created by An Nguyen on 28/03/2025.
+//
+
+import UIKit
+
+class PublishPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
